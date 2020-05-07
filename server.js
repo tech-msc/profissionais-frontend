6fdf8f49c10f7f9658a003aaa668ec898d5bc807
tech-msc <matheusscardoso5@github.com>
2020-05-07 07:30:03 -0300

    deploy heroku

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
+
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${nomeApp}`));
+// Serve os arquivos estáticos da pasta dist (gerada pelo ng build)
+app.use(express.static(__dirname + '/dist/profissionais-frontend'));
 
-app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${nomeApp}/index.html`));
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/profissionais-frontend/index.html'));
 });
 
+// Inicia a aplicação pela porta configurada
 app.listen(process.env.PORT || 8080);
