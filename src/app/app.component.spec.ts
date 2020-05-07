e1312bf7bf1d4d1154b1243e475a66399475c934
Matheus S. Cardoso <matheusscardoso5@github.com>
2020-05-07 07:44:31 -0300

    deploy heroku

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'profissionais-fontend'`, () => {
+  it(`should have as title 'profissionais-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('profissionais-fontend');
+    expect(app.title).toEqual('profissionais-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('profissionais-fontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('profissionais-frontend app is running!');
   });
 });
